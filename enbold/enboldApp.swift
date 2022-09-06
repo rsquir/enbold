c708372e5d5f4d8aa0889abba70c65be7bdd4210
Robert Squires <rsquir@gmail.com>
2022-09-06 13:35:14 -0600

    Initial Commit

---
+++ enbold/enboldApp.swift
@@ -0,0 +1,17 @@
+//
+//  enboldApp.swift
+//  enbold
+//
+//  Created by Robert Squires on 2022-09-06.
+//
+
+import SwiftUI
+
+@main
+struct enboldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
